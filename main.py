1d87813251d83ed8128fa0c7f1a48edb8e401e6b
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-03-08 16:32:03 -0400

    Create draft main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    return {'Hello': 'World'}
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: str | None = None):
+    return {'item_id': item_id, 'q':q}
+
+@app.get('/health')
+def health():
+    return {'errors': None}
