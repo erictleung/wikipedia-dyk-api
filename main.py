d28097c95c6e1bac6d9cc4684e8bb04c94c8dca2
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-03-09 02:40:58 -0400

    Use uvicorn to set up API

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+import uvicorn
 
 
 app = FastAPI()
 
-
 @app.get('/')
-def read_root():
-    return {'Hello': 'World'}
+async def root():
+    """
+    Root elements
+    """
+    return {'message': 'Hello, World!'}
 
 @app.get('/items/{item_id}')
-def read_item(item_id: int, q: str | None = None):
-    return {'item_id': item_id, 'q':q}
+def read_item(item_id: int):
+    return {'item_id': item_id}
 
 @app.get('/health')
 def health():
     return {'errors': None}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
