9b5e560a276050e049c8275954ee80e4c156ffdb
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-03-09 02:46:29 -0400

    Add documentation values

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ from fastapi import FastAPI
 import uvicorn
 
 
-app = FastAPI()
+app = FastAPI(
+    title='Wikipedia Did You Know KPI',
+    description='REST API for accessing Did You Know on Wikipedia since 2004',
+    version='1.0.0'
+)
 
-@app.get('/')
+@app.get(
+    '/',
+    summary='Base check',
+    description='Base check that API is working'
+)
 async def root():
-    """
-    Root elements
-    """
+    return {'message': 'Hello, World!'}
+
+@app.get(
+    '/all',
+    summary='Get all items',
+    description='Retrieve all Did You Know quotes and date information',
+    tags=['All']
+)
+async def get_all():
     return {'message': 'Hello, World!'}
 
 @app.get('/items/{item_id}')
